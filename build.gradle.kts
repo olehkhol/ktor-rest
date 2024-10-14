968f15a49e57682b61e5ae79df98ef023366d143
Oleh Kholiavchuk <oleh.khol@gmail.com>
2024-10-14 12:17:13 +0300

    Create unit tests with Ktor Client

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,13 +20,15 @@ repositories {
 }
 
 dependencies {
+    implementation(libs.ktor.client.content.negotiation)
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.host.common)
     implementation(libs.ktor.server.content.negotiation)
     implementation(libs.ktor.serialization.kotlinx.json)
     implementation(libs.ktor.server.netty)
-    implementation(libs.logback.classic)
     implementation(libs.ktor.server.config.yaml)
+    implementation(libs.logback.classic)
+
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
 }
